031435474252535b32bc710402b435526d0b448a
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-06-22 15:31:58 +0530

    [BACK/F#/Giraffe] Add route and action to update users

--- backends/fs-giraffe/Controllers/UsersController.fs
+++ backends/fs-giraffe/Controllers/UsersController.fs
@@ -1,7 +1,6 @@
 namespace Trendy.Controllers
 
 open FSharp.Control.Tasks
-open Microsoft.Extensions.Logging
 open Microsoft.AspNetCore.Http
 open Giraffe
 
@@ -15,55 +14,59 @@ module UsersController =
     open Trendy.Models
 
     [<CLIMutable>]
-    type GetParams = { Id: int }
+    type RouteParams = { Id: int }
 
     [<CLIMutable>]
-    type PostParams =
+    type BodyParams =
         { Id: int
           Name: string
           Email: string
           Password: string
           ConfirmPassword: string }
 
-    let userOfGetParams (context : LinksContext) (requestParams : GetParams) =
-        requestParams.Id |> context.Users.FindAsync |> Utils.optionTaskOfNullableValueTask
+    let userOfRouteParams (context: LinksContext) (requestParams: RouteParams) =
+        requestParams.Id
+        |> context.Users.FindAsync
+        |> Utils.optionTaskOfNullableValueTask
 
 
-    let userOfPostParams (requestParams : PostParams) : User.AllowedParams =
-        {
-            Id = -1
-            Name = requestParams.Name
-            Email = requestParams.Email
-            Password = requestParams.Password
-            ConfirmPassword = requestParams.ConfirmPassword
-        }
+    let userOfBodyParams (requestParams: BodyParams) : User.AllowedParams =
+        { Id = -1
+          Name = requestParams.Name
+          Email = requestParams.Email
+          Password = requestParams.Password
+          ConfirmPassword = requestParams.ConfirmPassword }
 
-    let show (requestParams: GetParams) : HttpHandler =
+    let userOfUpdateParams (id: int) (requestParams: BodyParams) : User.AllowedParams =
+        { Id = id
+          Name = requestParams.Name
+          Email = requestParams.Email
+          Password = requestParams.Password
+          ConfirmPassword = requestParams.ConfirmPassword }
+
+    let show (requestParams: RouteParams) : HttpHandler =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
                 let dbContext = ctx.GetService<LinksContext>()
-                match! requestParams |> userOfGetParams dbContext with
-                | Some user ->
-                    return! Successful.ok (json user) next ctx
-                | None ->
-                    return! RequestErrors.NOT_FOUND "Not Found." next ctx
+
+                match! requestParams |> userOfRouteParams dbContext with
+                | Some user -> return! Successful.ok (json user) next ctx
+                | None -> return! RequestErrors.NOT_FOUND "Not Found." next ctx
             }
 
     let create : HttpHandler =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
-                let logger = ctx.GetLogger<ILogger>()
                 let dbContext = ctx.GetService<LinksContext>()
-                let! requestParams = ctx.BindJsonAsync<PostParams>()
+                let! requestParams = ctx.BindJsonAsync<BodyParams>()
 
                 let validationResult =
                     requestParams
-                    |> userOfPostParams
+                    |> userOfBodyParams
                     |> UserValidation.validate dbContext
 
                 match validationResult with
-                | Error errors ->
-                    return! RequestErrors.unprocessableEntity (json errors) next ctx
+                | Error errors -> return! RequestErrors.unprocessableEntity (json errors) next ctx
                 | Ok validatedParams ->
                     validatedParams
                     |> User.userOfAllowedParams
@@ -74,7 +77,32 @@ module UsersController =
                     return! Successful.CREATED "" next ctx
             }
 
+    let update (requestParams: RouteParams) : HttpHandler =
+        fun (next: HttpFunc) (ctx: HttpContext) ->
+            task {
+                let dbContext = ctx.GetService<LinksContext>()
+                let! updateParams = ctx.BindJsonAsync<BodyParams>()
+
+                let validationResult =
+                    updateParams
+                    |> userOfUpdateParams requestParams.Id
+                    |> UserValidation.validate dbContext
+
+                match validationResult with
+                | Error errors -> return! RequestErrors.unprocessableEntity (json errors) next ctx
+                | Ok validatedParams ->
+                    validatedParams
+                    |> User.userOfAllowedParams
+                    |> dbContext.Users.Update
+                    |> ignore
+
+                    let! _ = dbContext.SaveChangesAsync()
+                    return! Successful.NO_CONTENT next ctx
+            }
+
 
     let router : HttpHandler =
-        choose [ GET >=> routeBind<GetParams> "/{id}(/?)" show
-                 POST >=> route "/" >=> create ]
+        choose [ GET >=> routeBind<RouteParams> "/{id}(/?)" show
+                 POST >=> route "/" >=> create
+                 PATCH
+                 >=> routeBind<RouteParams> "/{id}(/?)" update ]
