f900ab120ad58340ffb42cb78d4b97cbb8c430dc
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-07-05 07:41:19 +0530

    [BACK/F#/Giraffe] Validate that URL is indeed a URL

--- backends/fs-giraffe/Services/LinkValidation.fs
+++ backends/fs-giraffe/Services/LinkValidation.fs
@@ -1,7 +1,13 @@
 module Trendy.Services.LinkValidation
 
+open System.Text.RegularExpressions
+
+open Trendy.Utils
 open Trendy.Contexts.LinksContext
 open Trendy.Models
+
+/// Credit: @stephenhay's solution from https://mathiasbynens.be/demo/url-regex
+let urlRegex = @"^https?://[^\s/$.?#].[^\s]*$"
 
 let validateUrlPresence ({ Url = url }: Link.AllowedParams) =
     match url with
@@ -9,8 +15,17 @@ let validateUrlPresence ({ Url = url }: Link.AllowedParams) =
     | "" -> Error("Url", "Must be present")
     | _ -> Ok()
 
+let validateUrlFormat ({ Url = url }: Link.AllowedParams) =
+    let safeUrl = valueOrFallback url ""
+
+    match Regex.Match(safeUrl, urlRegex).Success with
+    | true -> Ok()
+    | false -> Error("Url", "Must be a valid URL")
+
 let validate (dbContext: LinksContext) (link: Link.AllowedParams) =
-    let validations = [ validateUrlPresence ]
+    let validations =
+        [ validateUrlPresence
+          validateUrlFormat ]
 
     let errors = Validation.validate validations link
 
